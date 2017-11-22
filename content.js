6eac58fe52543ae2242e2a72fc9757d287cac8f5
biggomega <benbotvinick@gmail.com>
2017-11-22 23:51:24 +0000

    Finished

--- content.js
+++ content.js
@@ -7,25 +7,46 @@ function getItems() {
 function saveData() {
   var title = document.getElementById('title').value;
   var date = document.getElementById('date').value;
-  var data = {};
-  data[title] = date;
-  chrome.storage.sync.set(data);
-  var status = document.getElementById('create');
-  status.textContent = 'Creating Assignment...';
-  setTimeout(function() {
-    status.textContent = 'Create Assignment';
-  }, 750);
+  if (title && date) {
+    var data = {};
+    data["AssignMe." + title] = date;
+    chrome.storage.sync.set(data);
+    var status = document.getElementById('create');
+    status.textContent = 'Creating...';
+    setTimeout(function() {
+      status.textContent = 'Create Assignment';
+    }, 1000);
+  } else if (!title && date) {
+    var status = document.getElementById('create');
+    status.textContent = 'Enter a title';
+    setTimeout(function() {
+      status.textContent = 'Create Assignment';
+    }, 1000);
+  } else if (title && !date) {
+    var status = document.getElementById('create');
+    status.textContent = 'Enter a date';
+    setTimeout(function() {
+      status.textContent = 'Create Assignment';
+    }, 1000);
+  } else if (!title && !date) {
+    var status = document.getElementById('create');
+    status.textContent = 'Enter a title and date';
+    setTimeout(function() {
+      status.textContent = 'Create Assignment';
+    }, 1000);
+  }
 }
 
-function removeAll() {
+function clearData() {
   chrome.storage.sync.clear(function() {
-    var error = chrome.runtime.lastError;
-    if (error) {
-      console.error(error);
-    }
+    var status = document.getElementById('clear');
+    status.textContent = 'Clearing...';
+    setTimeout(function() {
+      status.textContent = 'Create Assignment';
+    }, 1000);
   });
 }
 
 document.getElementById('create').addEventListener('click', saveData);
 document.getElementById('contents').addEventListener('click', getItems);
-document.getElementById('clear').addEventListener('click', removeAll);
+document.getElementById('clear').addEventListener('click', clearData);
--- display.js
+++ display.js
@@ -1,41 +1,25 @@
 toDo()
-function load() {
-    var channels = "";
-    var keywords = "";
-    chrome.storage.local.get('channels', function (result) {
-        channels = result.channels;
-        alert(result.channels);
-        $("#channels").val(channels);
-    });
-}
-//will run on page load
-document.addEventListener('DOMContentLoaded', load);
-
-/*
-toDo()
-function toDo() {//is this doing anything important?
-  //load values from the storage
-  chrome.storage.sync.get({
-    title: '',
-    date: ''
-  }, function(items) {
-    if(!!title && !!date){
-      alert(title + " - " + date)
-      var body = document.getElementById("yDmH0d").innerHTML;
-      if (body.search('<div><div class="T0FFIe rwnykc">AssignMe') === -1) {
-       var index = body.search('<div class="T0FFIe rwnykc">Due ');
-       var div = '<div><div class="T0FFIe rwnykc">AssignMe</div><div class="F0D56 DShyMc-NTA3MDM0NDY2NFpa"><div class="jjooHc yxp05b-Wvd9Cc"><div class="gWn3p"><div class="aCP5yb"><p>' + title + '</p></div><div class="h2eLLe Ya48ab"><a class="onkcGd BcZkCd">AssignMe</a><div class="ipPCc UZ2pse IMvYId">—&nbsp;Due ' + date + '</div></div></div><div class="ubVkr pQCS0d"></div></div></div></div>';
-      } else {
-       var index = body.search('<div class="F0D56 DShyMc-NTA3MDM0NDY2NFpa">');
-       var div = '<div class="F0D56 DShyMc-NTA3MDM0NDY2NFpa"><div class="jjooHc yxp05b-Wvd9Cc"><div class="gWn3p"><div class="aCP5yb"><p>' + title + '</p></div><div class="h2eLLe Ya48ab"><a class="onkcGd BcZkCd">AssignMe</a><div class="ipPCc UZ2pse IMvYId">—&nbsp;Due ' + date + '</div></div></div><div class="ubVkr pQCS0d"></div></div></div>';
+function toDo() {
+  chrome.storage.sync.get(null, function(items) {
+    for (var key in items) {
+      if (key.substring(0, 9) == 'AssignMe.') {
+        var title = key.substr(9);
+        var date = items[key];
+        var body = document.getElementById("yDmH0d").innerHTML;
+        if (body.search('<div><div class="T0FFIe rwnykc">AssignMe') === -1) {
+          var index = body.search('<div class="T0FFIe rwnykc">Due ');
+          var div = '<div><div class="T0FFIe rwnykc">AssignMe</div><div class="F0D56 DShyMc-NTA3MDM0NDY2NFpa"><div class="jjooHc yxp05b-Wvd9Cc"><div class="gWn3p"><div class="aCP5yb"><p class="onkcGd m8aQlb">' + title + '</p></div><div class="h2eLLe Ya48ab"><a class="onkcGd BcZkCd">AssignMe</a><div class="ipPCc UZ2pse IMvYId">—&nbsp;Due ' + date + '</div></div></div><div class="ubVkr pQCS0d"></div></div></div></div>';
+        } else {
+          var index = body.search('<div class="F0D56 DShyMc-NTA3MDM0NDY2NFpa">');
+          var div = '<div class="F0D56 DShyMc-NTA3MDM0NDY2NFpa"><div class="jjooHc yxp05b-Wvd9Cc"><div class="gWn3p"><div class="aCP5yb"><p>' + title + '</p></div><div class="h2eLLe Ya48ab"><a class="onkcGd BcZkCd">AssignMe</a><div class="ipPCc UZ2pse IMvYId">—&nbsp;Due ' + date + '</div></div></div><div class="ubVkr pQCS0d"></div></div></div>';
+        }
+        var output = [body.slice(0, index), div, body.slice(index)].join('');
+        if (document.location.href === 'https://classroom.google.com/u/0/a/not-turned-in/all' || document.location.href === 'https://classroom.google.com/a/not-turned-in/all' || document.location.href === 'file:///Users/benbotvinick/Desktop/GitHub/AssignMe/body.html') {
+          document.getElementById('yDmH0d').innerHTML = output;
+        }
       }
-      var output = [body.slice(0, index), div, body.slice(index)].join('');
-      if (document.location.href === 'https://classroom.google.com/u/0/a/not-turned-in/all' || document.location.href === 'https://classroom.google.com/a/not-turned-in/all') {
-       document.getElementById('yDmH0d').innerHTML = output;
-      }
-    }
+    };
   });
 }
-//will run on page load
+
 document.addEventListener('DOMContentLoaded', toDo);
-*/
